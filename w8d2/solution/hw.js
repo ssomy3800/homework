24b0a448068221aa6aafe2aff53ed3b387243f18
ssomy3800 <ssomy3800@gmail.com>
2023-05-03 04:18:44 -0700

    w9d3

--- w8d2/solution/hw.js
+++ w8d2/solution/hw.js
@@ -25,16 +25,16 @@ function mysteryScoping2() {
 
 mysteryScoping2(); // in block, out of block
 
-function mysteryScoping3() {
-  const x = "out of block";
-  if (true) {
-    var x = "in block";
-    console.log(x);
-  }
-  console.log(x);
-}
+// function mysteryScoping3() {
+//   const x = 'out of block';
+//   if (true) {
+//     var x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
 
-mysteryScoping3(); // SyntaxError
+// mysteryScoping3(); // SyntaxError
 
 function mysteryScoping4() {
   let x = "out of block";
@@ -47,19 +47,19 @@ function mysteryScoping4() {
 
 mysteryScoping4(); // in block, out of block
 
-function mysteryScoping5() {
-  let x = "out of block";
-  if (true) {
-    let x = "in block";
-    console.log(x);
-  }
-  let x = "out of block again";
-  console.log(x);
-}
+// function mysteryScoping5() {
+//   let x = "out of block";
+//   if (true) {
+//     let x = "in block";
+//     console.log(x);
+//   }
+//   let x = "out of block again";
+//   console.log(x);
+// }
 
-mysteryScoping5(); // SyntaxError
+// mysteryScoping5(); // SyntaxError
 
-// madLib
+// // madLib
 
 function madLib(verb, adj, noun) {
   return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`;
